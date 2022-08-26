952d7091dee607ac548fe5b433a76ccbcb2efd33
Alexis Charytonow <alexis@nodeux.com>
2022-08-26 04:04:11 -0500

    Setup Xcode Algorithms project.

---
+++ Algorithms/Algorithms/Executor.swift
@@ -0,0 +1,14 @@
+//
+//  Executor.swift
+//  Algorithms
+//
+//  Created by Alexis Charytonow on 8/26/22.
+//
+
+import Foundation
+
+class Executor {
+    func execute() {
+        print("Test!")
+    }
+}
---
+++ Algorithms/AlgorithmsTests/AlgorithmsTests.swift
@@ -0,0 +1,16 @@
+//
+//  AlgorithmsTests.swift
+//  AlgorithmsTests
+//
+//  Created by Alexis Charytonow on 8/26/22.
+//
+
+import XCTest
+@testable import Algorithms
+
+class AlgorithmsTests: XCTestCase {
+    func test_executor() {
+        let e = Executor()
+        e.execute()
+    }
+}
