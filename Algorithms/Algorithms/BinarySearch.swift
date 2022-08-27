eadc4a4bb3dd08f4a246fb58dde37162c3ff6158
Alexis Charytonow <alexis@nodeux.com>
2022-08-27 04:20:07 -0500

    Improvements for BinarySearch

--- Algorithms/Algorithms/BinarySearch.swift
+++ Algorithms/Algorithms/BinarySearch.swift
@@ -9,29 +9,18 @@ import Foundation
 
 class BinarySearch {
     static func binarySearch(arrayOfInts: [Int], target: Int) -> Int? {
-        let sizeOfArray = arrayOfInts.count
-        guard sizeOfArray != 0 else { return nil }
-        
         var lowerBound = 0
-        var upperBound = sizeOfArray - 1
+        var upperBound = arrayOfInts.count - 1
         
-        while (upperBound - lowerBound) > 1 {
+        while lowerBound <= upperBound {
             let index = Int(ceil((Double(lowerBound) + Double(upperBound)) / 2.0))
             if arrayOfInts[index] == target {
                 return index
             } else if arrayOfInts[index] < target {
-                lowerBound = index
+                lowerBound = index + 1
             } else {
-                upperBound = index
+                upperBound = index - 1
             }
-        }
-        
-        if arrayOfInts[lowerBound] == target {
-            return lowerBound
-        }
-        
-        if arrayOfInts[upperBound] == target {
-            return upperBound
         }
         
         return nil
